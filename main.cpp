c93ec857e2e6a737afdf5868c044ec78e9fc1ce1
Aasif Versi <versi786@gmail.com>
2019-12-26 12:13:16 -0500

    add dbus interface

--- main.cpp
+++ main.cpp
@@ -1,42 +1,63 @@
-#include <iostream>
+#include <dbus/dbus-glib.h>  // for dbus_g_*
+#include <dbus/dbus.h>       // for dbus_*
+#include <generated_dbus.h>
+#include <generated_mediaplayer2.h>
+#include <gio/gio.h>
 #include <glib.h>
 #include <glib/gprintf.h>
-#include <dbus/dbus.h> // for dbus_*
-#include <dbus/dbus-glib.h> // for dbus_g_*
-#include <gio/gio.h>
-#include <generated_mediaplayer2.h>
+#include <iostream>
+
+#define UNUSED(expr)  \
+    do {              \
+        (void)(expr); \
+    } while (0)
 
 int main(int argc, char **argv) {
-    gchar c = 'a';
-    std::cout << "Hello World\n" << c << std::endl;
+    UNUSED(argc);
+    UNUSED(argv);
 
-    OrgMprisMediaPlayer2Player *proxy;
     GError *error;
     error = NULL;
 
-    // Create the proxy
-    proxy = org_mpris_media_player2_player_proxy_new_for_bus_sync (
-            G_BUS_TYPE_SESSION,
-            G_DBUS_PROXY_FLAGS_NONE,
-            "org.mpris.MediaPlayer2.spotify",
-            "/org/mpris/MediaPlayer2",
-            NULL,
-            &error);
+    // List everything that supports the 'org.mpris.MediaPlayer2.player
+    // interface
+    OrgFreedesktopDBus *dbusProxy;
+    dbusProxy =
+        org_freedesktop_dbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION,
+                                                    G_DBUS_PROXY_FLAGS_NONE,
+                                                    "org.freedesktop.DBus",
+                                                    "/org/freedesktop/DBus",
+                                                    NULL,
+                                                    &error);
 
-    if(error) {
-        std::cout << error << std::endl;
+    if (error) {
+        std::cout << "Error: " << error->message << std::endl;
+    } else {
+        std::cout << "No error" << std::endl;
+    }
+
+    // Create the proxy
+    OrgMprisMediaPlayer2Player *playerProxy;
+    playerProxy = org_mpris_media_player2_player_proxy_new_for_bus_sync(
+        G_BUS_TYPE_SESSION,
+        G_DBUS_PROXY_FLAGS_NONE,
+        "org.mpris.MediaPlayer2.spotify",
+        "/org/mpris/MediaPlayer2",
+        NULL,
+        &error);
+
+    if (error) {
+        std::cout << "Error: " << error->message << std::endl;
     } else {
         std::cout << "No error" << std::endl;
     }
 
     // send play command
-    gboolean ret = org_mpris_media_player2_player_call_play_sync (
-    proxy,
-    NULL,
-    &error);
+    gboolean ret = org_mpris_media_player2_player_call_play_pause_sync(
+        playerProxy, NULL, &error);
 
-    if(error) {
-        std::cout << error << std::endl;
+    if (error) {
+        std::cout << "Error: " << error->message << std::endl;
     } else {
         std::cout << "No error" << std::endl;
     }
@@ -47,10 +68,7 @@ int main(int argc, char **argv) {
         std::cout << "ret is false" << std::endl;
     }
 
-
     // Cleanup the proxy
-    g_object_unref(proxy);
+    g_object_unref(playerProxy);
 }
-
-
 
